8ae1d40dd603f9ac82262e08326519661db46450
pgrunm <8317771+pgrunm@users.noreply.github.com>
2025-01-31 14:53:42 +0100

    Add func get last monday.

--- calendar_week.go
+++ calendar_week.go
@@ -1,7 +1,14 @@
 package main
 
-import "time"
+import (
+	"time"
+)
 
 func getCalendarWeek(t time.Time) (year int, week int) {
 	return time.Time.ISOWeek(t)
+}
+
+// Returns the date of the last monday for t.
+func getLastMonday(t time.Time) time.Time {
+	return t.AddDate(0, 0, (-int(t.Weekday()) + 1))
 }
