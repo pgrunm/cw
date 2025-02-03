8071967d739b01afba4b500763d2a4c0a54896bc
pgrunm <8317771+pgrunm@users.noreply.github.com>
2025-02-03 09:37:55 +0100

    Add commandline flag, to enable summary. Move to new default output.

--- main.go
+++ main.go
@@ -10,16 +10,30 @@ import (
 )
 
 func main() {
+	// summary contains a boolean, whether to output a short summary.
+	var summary bool
 	app := &cli.App{
 		Name:  "cw",
 		Usage: "Find the appropriate calendar week of a given date.",
-		Action: func(*cli.Context) error {
-
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "summary",
+				Aliases:     []string{"s"},
+				Value:       false,
+				Usage:       "Print out the calendar week in a short summary.",
+				Destination: &summary,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			var formattedWeek string
 			currentTime := time.Now()
 			year, week := getCalendarWeek(currentTime)
-
+			if summary {
+				formattedWeek = fmt.Sprintf("It's currently calendar week %d in %d.", week, year)
+			} else {
+				formattedWeek = fmt.Sprintf("%d", week)
+			}
 			// Print the current calendar week
-			formattedWeek := fmt.Sprintf("It's currently calendar week %d in %d.", week, year)
 			fmt.Println(formattedWeek)
 			return nil
 		},
