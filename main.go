98ada3e07a9f1a8d88a79b50f8467110c4ddffc2
Warryz <8317771+Warryz@users.noreply.github.com>
2025-03-06 12:53:52 +0100

    Add json output format.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -30,10 +31,10 @@ func main() {
 			&cli.StringFlag{
 				Name:    "output",
 				Aliases: []string{"o"},
-				Usage:   "Prints the requested calendar week as JSON or YAML.",
+				Usage:   "Prints the requested calendar week as JSON.",
 				Action: func(cCtx *cli.Context, s string) error {
 					if s != "json" && s != "yaml" {
-						return fmt.Errorf("Output format must be either 'json' or 'yaml', got '%s'.", s)
+						return fmt.Errorf("Output format must be 'json', got '%s'.", s)
 					}
 					return nil
 				},
@@ -44,11 +45,34 @@ func main() {
 			var formattedWeek string
 			currentTime := time.Now()
 			year, week := getCalendarWeek(currentTime)
-			if summary {
-				monday := getLastMonday(currentTime)
-				formattedWeek = fmt.Sprintf("It's currently calendar week %d in %d, which started on %s and will finish on %s.", week, year, monday.Format(time.DateOnly), monday.AddDate(0, 0, 7).Format(time.DateOnly))
-			} else {
-				formattedWeek = fmt.Sprintf("%d", week)
+			monday := getLastMonday(currentTime)
+			weekData := map[string]interface{}{
+				"year":  year,
+				"week":  week,
+				"start": monday.Format(time.DateOnly),
+				"end":   monday.AddDate(0, 0, 7).Format(time.DateOnly),
+			}
+
+			// If not summary is required, remove the data from the map.
+			if !summary {
+				delete(weekData, "year")
+				delete(weekData, "start")
+				delete(weekData, "end")
+			}
+
+			switch output {
+			case "json":
+				jsonData, err := json.MarshalIndent(weekData, "", "  ")
+				if err != nil {
+					return fmt.Errorf("failed to marshal JSON: %v", err)
+				}
+				formattedWeek = string(jsonData)
+			default:
+				if summary {
+					formattedWeek = fmt.Sprintf("It's currently calendar week %d in %d, which started on %s and will finish on %s.", week, year, monday.Format(time.DateOnly), monday.AddDate(0, 0, 7).Format(time.DateOnly))
+				} else {
+					formattedWeek = fmt.Sprintf("%d", week)
+				}
 			}
 			// Print the current calendar week
 			fmt.Println(formattedWeek)
