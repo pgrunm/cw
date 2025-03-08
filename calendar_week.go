33c74660e5f558d1ee2df5f6afcb4119ca91a793
Warryz <8317771+Warryz@users.noreply.github.com>
2025-03-08 11:18:22 +0100

    Simplify code.

--- calendar_week.go
+++ calendar_week.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -18,4 +21,53 @@ func getCalendarWeek(t time.Time) (year int, week int) {
 // Returns the date of the last monday for t.
 func getLastMonday(t time.Time) time.Time {
 	return t.AddDate(0, 0, (-int(t.Weekday()) + 1))
+}
+
+func parseRequestedDate(submittedDate string) (time.Time, error) {
+	if submittedDate == "" {
+		return time.Now(), nil
+	}
+
+	// Parse the submitted calendar week from string to int
+	week, err := strconv.Atoi(submittedDate)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return time.Date(time.Now().Year(), 1, 1, 0, 0, 0, 0, time.Local).AddDate(0, 0, (week-1)*7), nil
+}
+
+func getWeekOutput(params calendarParams, currentTime time.Time) (formattedWeek string, err error) {
+	year, week := getCalendarWeek(currentTime)
+	monday := getLastMonday(currentTime)
+	weekData := map[string]interface{}{
+		"year":  year,
+		"week":  week,
+		"start": monday.Format(time.DateOnly),
+		"end":   monday.AddDate(0, 0, 7).Format(time.DateOnly),
+	}
+
+	// If not summary is required, remove the data from the map.
+	if !params.summary {
+		delete(weekData, "year")
+		delete(weekData, "start")
+		delete(weekData, "end")
+	}
+
+	switch params.output {
+	case "json":
+		jsonData, jsonErr := json.Marshal(weekData)
+		if jsonErr != nil {
+			return "", fmt.Errorf("failed to marshal JSON: %v", err)
+		}
+		formattedWeek = string(jsonData)
+	default:
+		if params.summary {
+			formattedWeek = fmt.Sprintf("It's currently calendar week %d in %d, which started on %s and will finish on %s.",
+				week, year, monday.Format(time.DateOnly), monday.AddDate(0, 0, 7).Format(time.DateOnly))
+		} else {
+			formattedWeek = fmt.Sprintf("%d", week)
+		}
+	}
+	return formattedWeek, nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"time"
 
 	"github.com/urfave/cli/v2"
 )
@@ -41,58 +38,18 @@ func main() {
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
-			var formattedWeek string
-			var currentTime time.Time
-
 			// Parse any entered calendar week as flag from commandline
 			submittedDate := cCtx.Args().First()
-			if submittedDate != "" {
-				// Parse the submitted calendar week from string to int
-				week, err := strconv.Atoi(submittedDate)
-				if err != nil {
-					return fmt.Errorf(
-						"Failed to parse the submitted calendar week %s, please enter an integer value. Error: %s",
-						submittedDate, err)
-				}
-				params.summary = true
-				currentTime = time.Date(time.Now().Year(), 1, 1, 0, 0, 0, 0, time.Local).AddDate(0, 0, (week-1)*7)
-			} else {
-				currentTime = time.Now()
-			}
-
-			year, week := getCalendarWeek(currentTime)
-			monday := getLastMonday(currentTime)
-			weekData := map[string]interface{}{
-				"year":  year,
-				"week":  week,
-				"start": monday.Format(time.DateOnly),
-				"end":   monday.AddDate(0, 0, 7).Format(time.DateOnly),
-			}
-
-			// If not summary is required, remove the data from the map.
-			if !params.summary {
-				delete(weekData, "year")
-				delete(weekData, "start")
-				delete(weekData, "end")
-			}
-
-			switch params.output {
-			case "json":
-				jsonData, err := json.Marshal(weekData)
-				if err != nil {
-					return fmt.Errorf("failed to marshal JSON: %v", err)
-				}
-				formattedWeek = string(jsonData)
-			default:
-				if params.summary {
-					formattedWeek = fmt.Sprintf("It's currently calendar week %d in %d, which started on %s and will finish on %s.",
-						week, year, monday.Format(time.DateOnly), monday.AddDate(0, 0, 7).Format(time.DateOnly))
-				} else {
-					formattedWeek = fmt.Sprintf("%d", week)
-				}
+			currentTime, err := parseRequestedDate(submittedDate)
+			if err != nil {
+				return fmt.Errorf("failed to parse the requested date: %v", err)
 			}
 			// Print the current calendar week
-			fmt.Println(formattedWeek)
+			formattedWeek, err := getWeekOutput(params, currentTime)
+			if err != nil {
+				return fmt.Errorf("failed to get week output: %v", err)
+			}
+			print(formattedWeek)
 			return nil
 		},
 	}
