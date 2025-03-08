1e1343f7a5be9320ac31d1edcba4bcfe75094fdc
Warryz <8317771+Warryz@users.noreply.github.com>
2025-03-08 11:18:04 +0100

    Add new test.

--- calendar_week_test.go
+++ calendar_week_test.go
@@ -54,3 +54,38 @@ func TestLastMonday(t *testing.T) {
 		})
 	}
 }
+
+func TestWeekOutput(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		params   calendarParams
+		expected string
+		time     time.Time
+	}{
+		{
+			desc: "Check week output w summary",
+			params: calendarParams{
+				summary: true,
+			},
+			time:     time.Date(2025, 1, 27, 8, 30, 0, 0, time.UTC),
+			expected: "It's currently calendar week 5 in 2025, which started on 2025-01-27 and will finish on 2025-02-03.",
+		},
+		{
+			desc: "Check week output wo summary",
+			params: calendarParams{
+				summary: false,
+				output:  "text",
+			},
+			time:     time.Date(2025, 1, 27, 8, 30, 0, 0, time.UTC),
+			expected: "5",
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			actual, _ := getWeekOutput(tC.params, tC.time)
+			if actual != tC.expected {
+				t.Errorf("Expected %s, got %s", tC.expected, actual)
+			}
+		})
+	}
+}
